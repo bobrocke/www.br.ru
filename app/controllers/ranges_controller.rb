b1644f9128fb5bb1862439fb908e55f37b0424bd
Bob Rockefeller <bob@bobrockefeller.com>
2025-01-23 11:44:58 -0500

    One more…

--- app/controllers/ranges_controller.rb
+++ app/controllers/ranges_controller.rb
@@ -1,8 +1,20 @@
 class RangesController < ApplicationController
+  # def initialize
+  #   @the_color_hex = "#888888"
+  # end
+
   def ranges
-    puts "the_color_hex"
+    # @the_color_hex = "#888888"
+    @the_color_as_hex = @the_color_hex
+    logger.info @the_color_as_hex
+    logger.info @the_color_hex
   end
 
   def set_color_hex
+    @the_color_hex = params[:the_color_hex]
+
+    logger.info @the_color_hex
+
+    render :ranges
   end
 end
